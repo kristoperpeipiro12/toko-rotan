62c0d8517ada670ae4918a2ccf4787e8aa6f8f7c
unknown <yedutun19@gmail.com>
2025-03-13 14:40:17 +0700

    lanjut validasi request dari Detail>CO dan Cart>CO

--- resources/views/shop/co.blade.php
+++ resources/views/shop/co.blade.php
@@ -12,6 +12,9 @@
     <div class="co-page">
         <div class="navbar-co"><span class="navbar-brand" id="keluar">AL - ZAHRA</span></div>
 
+        @if (filled($produk_pesanan))
+        {{ "hehe" }}
+        @endif
         <div class="wrap-container-co">
             <div class="container co-page summary-section">
                 <header class="header co-page">
@@ -22,15 +25,15 @@
                     <div class="section-header co-page">ALAMAT PENGIRIMAN</div>
                     <div class="address-details co-page" id="alamat-terpilih">
                         @if ($penerima)
-                            <p><strong id="alamat-lokasi">{{ $penerima->lokasi }} &bull;
-                                    {{ $penerima->nama_penerima }}</strong></p>
-                            <p id="alamat-detail">
-                                {{ $penerima->alamat }}, {{ $penerima->nohp_penerima }}
-                            </p>
-                            <span class="change-btn" id="ganti-alamat">Ganti</span>
+                        <p><strong id="alamat-lokasi">{{ $penerima->lokasi }} &bull;
+                                {{ $penerima->nama_penerima }}</strong></p>
+                        <p id="alamat-detail">
+                            {{ $penerima->alamat }}, {{ $penerima->nohp_penerima }}
+                        </p>
+                        <span class="change-btn" id="ganti-alamat">Ganti</span>
                         @else
-                            <p id="alamat-kosong">Alamat belum ada</p>
-                            <button id="btn-tambah">Tambah</button>
+                        <p id="alamat-kosong">Alamat belum ada</p>
+                        <button id="btn-tambah">Tambah</button>
                         @endif
                     </div>
                 </section>
@@ -39,28 +42,30 @@
                 <div class="checkout-item">
                     <span>Daftar Produk: </span>
                 </div>
+                @if (!filled($produk_pesanan))
                 @foreach ($cartItems as $item)
-                    <section class="order-summary co-page">
-                        <div class="section-header co-page" style="font-size: 1.8rem; font-weight: 700;">
-                            {{ $item->produk_varian->produk->nama_produk }}
+                <section class="order-summary co-page">
+                    <div class="section-header co-page" style="font-size: 1.8rem; font-weight: 700;">
+                        {{ $item->produk_varian->produk->nama_produk }}
+                    </div>
+                    <div class="order-item co-page">
+                        <img src="storage/{{ $item->produk_varian->gambar }}" alt="Product Image" />
+                        <div class="order-info co-page">
+                            <h4 style="display: flex; flex-direction: column; gap: 5px; margin-bottom: 15px;">
+                                <span
+                                    style="font-size: 1.5rem; font-weight: 300;">{{ $item->produk_varian->warna }}</span>
+                                <span
+                                    style="font-style: italic; font-size: 1rem; font-weight: 300;">{{ $item->produk_varian->produk->deskripsi }}</span>
+                            </h4>
+                            <p>Ukuran: {{ $item->produk_varian->ukuran }}</p>
+                            <p class="quantity-price">{{ $item->jumlah }}pcs x Rp.
+                                {{ number_format($item->produk_varian->harga, 0, ',', '.') }}
+                            </p>
                         </div>
-                        <div class="order-item co-page">
-                            <img src="storage/{{ $item->produk_varian->gambar }}" alt="Product Image" />
-                            <div class="order-info co-page">
-                                <h4 style="display: flex; flex-direction: column; gap: 5px; margin-bottom: 15px;">
-                                    <span
-                                        style="font-size: 1.5rem; font-weight: 300;">{{ $item->produk_varian->warna }}</span>
-                                    <span
-                                        style="font-style: italic; font-size: 1rem; font-weight: 300;">{{ $item->produk_varian->produk->deskripsi }}</span>
-                                </h4>
-                                <p>Ukuran: {{ $item->produk_varian->ukuran }}</p>
-                                <p class="quantity-price">{{ $item->jumlah }}pcs x Rp.
-                                    {{ number_format($item->produk_varian->harga, 0, ',', '.') }}
-                                </p>
-                            </div>
-                        </div>
-                    </section>
+                    </div>
+                </section>
                 @endforeach
+                @endif
 
             </div>
 
@@ -70,12 +75,16 @@
                         <h1 class="header-rincian">Cek ringkasan transaksi!</h1>
                         <div class="ringkasan-row">
                             <span>Total Harga</span>
+                            @if (!filled($produk_pesanan))
                             <span class="biaya">Rp. {{ number_format($total_harga, 0, ',', '.') }}</span>
+                            @endif
                         </div>
 
                         <div class="ringkasan-row">
                             <span>Ongkos Kirim</span>
+                            @if (!filled($produk_pesanan))
                             <span class="biaya">Rp. {{ number_format($ongkir, 0, ',', '.') }}</span>
+                            @endif
                         </div>
 
                         <span style="font-style: italic; margin-bottom: 10px;">sistem pembayaran COD (Cash On
@@ -85,13 +94,17 @@
                     <div class="ringkasan-total">
                         <div class="ringkasan-row">
                             <span class="net-total">Total Tagihan</span>
+                            @if (!filled($produk_pesanan))
                             <span class="net-total">Rp. {{ number_format($total_tagihan, 0, ',', '.') }}</span>
+                            @endif
                         </div>
 
                         <form action="{{ route('pesan.proses') }}" method="POST">
                             @csrf
+                            @if (!filled($produk_pesanan))
                             <input type="hidden" name="selected_items"
                                 value="{{ implode(',', $cartItems->pluck('id_keranjang')->toArray()) }}">
+                            @endif
                             <div class="btn-co-bayar-sekarang">
                                 <button type="submit" style="background: none; border: none; cursor: pointer;">Pesan
                                     Sekarang</button>
@@ -130,29 +143,29 @@
 
             <form id="formPilihAlamat">
                 @foreach ($alamat as $pn)
-                    <div class="container-alamat">
-                        <input type="radio" name="pilih_alamat" value="{{ $pn->id_penerima }}"
-                            data-lokasi="{{ $pn->lokasi }}" data-nama="{{ $pn->nama_penerima }}"
-                            data-alamat="{{ $pn->alamat }}" data-nohp="{{ $pn->nohp_penerima }}"
-                            style="display: inline; background-color: aqua;" />
-                        <div class="bg-primary d-flex flex-column">
-                            <label>
-                                <div class="header-alamat">{{ $pn->lokasi }}</div>
-                                <div class="content-alamat">
-                                    <span>Nama : {{ $pn->nama_penerima }}</span>
-                                    <span>Alamat : {{ $pn->alamat }}</span>
-                                    <span>No. Handphone : {{ $pn->nohp_penerima }}</span>
-                                </div>
-                            </label>
-                            <p class="btn-ubah-alamat" role="button" tabindex="0" data-id="{{ $pn->id_penerima }}"
-                                data-lokasi="{{ $pn->lokasi }}" data-alamat="{{ $pn->alamat }}"
-                                data-nama="{{ $pn->nama_penerima }}" data-nohp="{{ $pn->nohp_penerima }}">
-                                Ubah Alamat
-                            </p>
-                        </div>
-
-
+                <div class="container-alamat">
+                    <input type="radio" name="pilih_alamat" value="{{ $pn->id_penerima }}"
+                        data-lokasi="{{ $pn->lokasi }}" data-nama="{{ $pn->nama_penerima }}"
+                        data-alamat="{{ $pn->alamat }}" data-nohp="{{ $pn->nohp_penerima }}"
+                        style="display: inline; background-color: aqua;" />
+                    <div class="bg-primary d-flex flex-column">
+                        <label>
+                            <div class="header-alamat">{{ $pn->lokasi }}</div>
+                            <div class="content-alamat">
+                                <span>Nama : {{ $pn->nama_penerima }}</span>
+                                <span>Alamat : {{ $pn->alamat }}</span>
+                                <span>No. Handphone : {{ $pn->nohp_penerima }}</span>
+                            </div>
+                        </label>
+                        <p class="btn-ubah-alamat" role="button" tabindex="0" data-id="{{ $pn->id_penerima }}"
+                            data-lokasi="{{ $pn->lokasi }}" data-alamat="{{ $pn->alamat }}"
+                            data-nama="{{ $pn->nama_penerima }}" data-nohp="{{ $pn->nohp_penerima }}">
+                            Ubah Alamat
+                        </p>
                     </div>
+
+
+                </div>
                 @endforeach
             </form>
         </div>
@@ -188,8 +201,8 @@
 
                     <div class="wrap-content-ubah">
                         <label for="nama_penerima">Nama Penerima:</label>
-                        <input type="text" id="nama_penerima" name="nama_penerima"
-                            placeholder="Masukkan Nama Penerima" required />
+                        <input type="text" id="nama_penerima" name="nama_penerima" placeholder="Masukkan Nama Penerima"
+                            required />
                     </div>
 
                     <div class="wrap-content-ubah">
@@ -212,37 +225,37 @@
     <script src="https://kit.fontawesome.com/b902581f05.js" crossorigin="anonymous"></script>
 
     <script>
-        document.querySelectorAll('input[name="pilih_alamat"]').forEach((radio) => {
-            radio.addEventListener("change", function() {
-                let lokasi = this.dataset.lokasi;
-                let nama = this.dataset.nama;
-                let alamat = this.dataset.alamat;
-                let nohp = this.dataset.nohp;
+    document.querySelectorAll('input[name="pilih_alamat"]').forEach((radio) => {
+        radio.addEventListener("change", function() {
+            let lokasi = this.dataset.lokasi;
+            let nama = this.dataset.nama;
+            let alamat = this.dataset.alamat;
+            let nohp = this.dataset.nohp;
 
-                // Update tampilan alamat
-                document.getElementById("alamat-lokasi").innerHTML = `${lokasi} • ${nama}`;
-                document.getElementById("alamat-detail").innerHTML = `${alamat}, ${nohp}`;
+            // Update tampilan alamat
+            document.getElementById("alamat-lokasi").innerHTML = `${lokasi} • ${nama}`;
+            document.getElementById("alamat-detail").innerHTML = `${alamat}, ${nohp}`;
 
-                let alamatKosong = document.getElementById("alamat-kosong");
-                if (alamatKosong) {
-                    alamatKosong.style.display = "none";
-                }
+            let alamatKosong = document.getElementById("alamat-kosong");
+            if (alamatKosong) {
+                alamatKosong.style.display = "none";
+            }
 
-                document.getElementById("wrap-popup-co").style.display = "none";
-            });
-        });
-
-        document.getElementById("ganti-alamat").addEventListener("click", function() {
-            document.getElementById("wrap-popup-co").style.display = "flex";
-        });
-        document.getElementById("close-alamat").addEventListener("click", function() {
             document.getElementById("wrap-popup-co").style.display = "none";
         });
+    });
+
+    document.getElementById("ganti-alamat").addEventListener("click", function() {
+        document.getElementById("wrap-popup-co").style.display = "flex";
+    });
+    document.getElementById("close-alamat").addEventListener("click", function() {
+        document.getElementById("wrap-popup-co").style.display = "none";
+    });
     </script>
     <script>
-        document.getElementById("btn-tambah").addEventListener("click", function() {
-            window.location.href = "{{ route('cs.account') }}";
-        });
+    document.getElementById("btn-tambah").addEventListener("click", function() {
+        window.location.href = "{{ route('cs.account') }}";
+    });
     </script>
 </body>
 
--- resources/views/shop/detail.blade.php
+++ resources/views/shop/detail.blade.php
@@ -1,151 +1,150 @@
 @extends('shop.layouts.app')
 @section('title', 'Detail')
 @section('content')
-    @include('sweetalert::alert')
+@include('sweetalert::alert')
 
-    <div class="custom-alert-detail" id="alert-detail">
-        <span>Harap pilih ukuran terlebih dahulu!</span>
-    </div>
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            const ukuranItems = document.querySelectorAll(".ukuran-item");
-            const selectedSlugInput = document.getElementById("selected_slug");
-            const alertDetail = document.getElementById("alert-detail");
+<div class="custom-alert-detail" id="alert-detail">
+    <span>Harap pilih ukuran terlebih dahulu!</span>
+</div>
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    const ukuranItems = document.querySelectorAll(".ukuran-item");
+    const selectedSlugInput = document.getElementById("selected_slug");
+    const alertDetail = document.getElementById("alert-detail");
 
-            let selectedUkuran = null;
+    let selectedUkuran = null;
 
-            ukuranItems.forEach(item => {
-                item.addEventListener("click", function() {
-                    ukuranItems.forEach(uk => uk.classList.remove("active"));
-                    this.classList.add("active");
+    ukuranItems.forEach(item => {
+        item.addEventListener("click", function() {
+            ukuranItems.forEach(uk => uk.classList.remove("active"));
+            this.classList.add("active");
 
-                    selectedSlugInput.value = this.getAttribute("data-slug");
-                    selectedUkuran = this.getAttribute("data-slug");
-                });
-            });
-
-            document.getElementById("form-cart-hidden").addEventListener("submit", function(event) {
-                if (!selectedUkuran) {
-                    event.preventDefault();
-                    alertDetail.classList.add("active");
-
-                    setTimeout(() => {
-                        alertDetail.classList.remove("active");
-                    }, 3000);
-                }
-            });
+            selectedSlugInput.value = this.getAttribute("data-slug");
+            selectedUkuran = this.getAttribute("data-slug");
         });
-    </script>
+    });
 
-    @php
-        session()->put('slug_detail', $varian->slug);
-    @endphp
+    document.getElementById("form-cart-hidden").addEventListener("submit", function(event) {
+        if (!selectedUkuran) {
+            event.preventDefault();
+            alertDetail.classList.add("active");
 
-    <div class="wrap-detail content-border">
-        <div class="container-pdt-dt1">
-            <a href="{{ route('shop.shop') }}" class="btn mb-3"><i class="fa-solid fa-arrow-left"></i> Kembali</a>
-            <div class="product-header-pdt-dt1">
-                <div class="product-image-pdt-dt1">
-                    <img src="{{ asset('storage/' . $produk->gambar) }}" alt="Gambar Produk" class="card-img-c5"
-                        onerror="this.onerror=null; this.src='{{ asset('images/default-product.jpg') }}';" />
+            setTimeout(() => {
+                alertDetail.classList.remove("active");
+            }, 3000);
+        }
+    });
+});
+</script>
+
+@php
+session()->put('slug_detail', $varian->slug);
+@endphp
+
+<div class="wrap-detail content-border">
+    <div class="container-pdt-dt1">
+        <a href="{{ route('shop.shop') }}" class="btn mb-3"><i class="fa-solid fa-arrow-left"></i> Kembali</a>
+        <div class="product-header-pdt-dt1">
+            <div class="product-image-pdt-dt1">
+                <img src="{{ asset('storage/' . $produk->gambar) }}" alt="Gambar Produk" class="card-img-c5"
+                    onerror="this.onerror=null; this.src='{{ asset('images/default-product.jpg') }}';" />
+            </div>
+            <div class="product-details-pdt-dt1">
+                <div class="product-title-pdt-dt1">{{ $produk->produk->nama_produk }}</div>
+                <div class="product-color-selected-pdt-dt1">{{ $produk->warna }}</div>
+                <div id="product-price" class="product-price-pdt-dt1">Rp.
+                    {{ number_format($produk->harga, 0, ',', '.') }}</div>
+                <div class="product-description-pdt-dt1">
+                    {{ $produk->produk->deskripsi }}
                 </div>
-                <div class="product-details-pdt-dt1">
-                    <div class="product-title-pdt-dt1">{{ $produk->produk->nama_produk }}</div>
-                    <div class="product-color-selected-pdt-dt1">{{ $produk->warna }}</div>
-                    <div id="product-price" class="product-price-pdt-dt1">Rp.
-                        {{ number_format($produk->harga, 0, ',', '.') }}</div>
-                    <div class="product-description-pdt-dt1">
-                        {{ $produk->produk->deskripsi }}
-                    </div>
-                    <div class="options-pdt-dt1">
-                        <div class="option-group-pdt-dt1">
-                            <label for="size-pdt-dt1">Pilih Ukuran:</label>
-                            <div class="wrap-uk-detail">
-                                @foreach ($ukuran as $uk)
-                                    <a href="javascript:void(0)">
-                                        <div class="ukuran-item {{ $uk['ukuran'] == $selectedUkuran ? 'active' : '' }}"
-                                            data-slug="{{ $uk->slug }}" data-harga="{{ $uk->harga }}"
-                                            data-stok="{{ $uk->stok }}">
-                                            {{ $uk->ukuran }}
-                                        </div>
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
-
-
-                        <div class="option-group-pdt-dt1">
-                            <label for="color-pdt-dt1">Pilih Warna:</label>
-                            <div class="wrap-warna-detail">
-                                @foreach ($result as $r)
-                                    <a href="{{ $r['slug'] }}"
-                                        class="card-protal-warna {{ $r['warna'] == $warna_selected ? 'active' : '' }}"
-                                        id="selected-portal-warna">
-                                        <img class="img-portal-warna" width="70px"
-                                            src="{{ asset('storage/' . $r['gambar']) }}" alt="gambar-produk">
-                                        <span>{{ $r['warna'] }}</span>
-                                    </a>
-                                @endforeach
-                            </div>
-                        </div>
-
-                        <div class="option-group-pdt-dt1">
-                            <label for="quantity">Jumlah:</label>
-                            <div class="quantity-container">
-                                <button type="button" id="decrease">-</button>
-                                <input type="text" id="quantity" value="1">
-                                <button type="button" id="increase">+</button>
-                                <span>Stok: <strong id="stock-amount">{{ $produk->stok }}</strong></span>
-                            </div>
+                <div class="options-pdt-dt1">
+                    <div class="option-group-pdt-dt1">
+                        <label for="size-pdt-dt1">Pilih Ukuran:</label>
+                        <div class="wrap-uk-detail">
+                            @foreach ($ukuran as $uk)
+                            <a href="javascript:void(0)">
+                                <div class="ukuran-item {{ $uk['ukuran'] == $selectedUkuran ? 'active' : '' }}"
+                                    data-slug="{{ $uk->slug }}" data-harga="{{ $uk->harga }}"
+                                    data-stok="{{ $uk->stok }}">
+                                    {{ $uk->ukuran }}
+                                </div>
+                            </a>
+                            @endforeach
                         </div>
                     </div>
-                    <form id="form-cart-hidden" action="{{ route('shop.cart.store') }}" method="POST">
-                        @csrf
-                        {{-- hidden input --}}
-                        <input type="hidden" name="jumlah_pesanan" id="jumlah_pesan">
-                        <input type="hidden" name="selected_slug" id="selected_slug">
-                        <input type="hidden" name="go_to_cart">
-                        <button type="submit" id="tambah-keranjang"
-                            class="text-decoration-none text-center add-to-cart-btn-pdt-dt1">
-                            Tambahkan ke Keranjang
-                        </button>
-                    </form>
 
-                    <form action="{{ route('shop.co') }}" method="POST">
-                        @csrf
-                        <input type="hidden" name="go_to_co">
-                        <input type="hidden" name="produk_varian" value="{{ $produk->id_varian }}"
-                            id="selected-items-input">
-                        <input type="hidden" name="jumlah_pesanan" id="jumlah_pesan">
-                        <button type="submit" class="checkout-btn">Checkout</button>
-                    </form>
 
+                    <div class="option-group-pdt-dt1">
+                        <label for="color-pdt-dt1">Pilih Warna:</label>
+                        <div class="wrap-warna-detail">
+                            @foreach ($result as $r)
+                            <a href="{{ $r['slug'] }}"
+                                class="card-protal-warna {{ $r['warna'] == $warna_selected ? 'active' : '' }}"
+                                id="selected-portal-warna">
+                                <img class="img-portal-warna" width="70px" src="{{ asset('storage/' . $r['gambar']) }}"
+                                    alt="gambar-produk">
+                                <span>{{ $r['warna'] }}</span>
+                            </a>
+                            @endforeach
+                        </div>
+                    </div>
+
+                    <div class="option-group-pdt-dt1">
+                        <label for="quantity">Jumlah:</label>
+                        <div class="quantity-container">
+                            <button type="button" id="decrease">-</button>
+                            <input type="text" id="quantity" value="1">
+                            <button type="button" id="increase">+</button>
+                            <span>Stok: <strong id="stock-amount">{{ $produk->stok }}</strong></span>
+                        </div>
+                    </div>
                 </div>
+                <form id="form-cart-hidden" action="{{ route('shop.cart.store') }}" method="POST">
+                    @csrf
+                    {{-- hidden input --}}
+                    <input type="hidden" name="jumlah_pesanan" id="jumlah_pesan">
+                    <input type="hidden" name="selected_slug" id="selected_slug">
+                    <button type="submit" id="tambah-keranjang"
+                        class="text-decoration-none text-center add-to-cart-btn-pdt-dt1">
+                        Tambahkan ke Keranjang
+                    </button>
+                </form>
+
+                <form action="{{ route('shop.co') }}" method="POST">
+                    @csrf
+                    <input type="hidden" name="go_to_co">
+                    <input type="hidden" name="produk_varian" value="{{ $produk->id_varian }}"
+                        id="selected-items-input">
+                    <input type="hidden" name="jumlah_pesanan" id="jumlah_pesan">
+                    <button type="submit" class="checkout-btn">Checkout</button>
+                </form>
+
             </div>
         </div>
-
-    </div>
     </div>
 
-    <script src="{{ asset('assets/js/shop/detail-page.js') }}"></script>
-    <script>
-        // Kirim slug ke backend
-        document.addEventListener("DOMContentLoaded", function() {
-            let jumlahPesan = document.getElementById("jumlah_pesan");
-            let selectedSlug = document.getElementById("selected_slug");
+</div>
+</div>
 
-            document.querySelectorAll(".ukuran-item").forEach(function(item) {
-                item.addEventListener("click", function() {
-                    let slug = item.getAttribute("data-slug");
-                    selectedSlug.value = slug;
-                });
-            });
+<script src="{{ asset('assets/js/shop/detail-page.js') }}"></script>
+<script>
+// Kirim slug ke backend
+document.addEventListener("DOMContentLoaded", function() {
+    let jumlahPesan = document.getElementById("jumlah_pesan");
+    let selectedSlug = document.getElementById("selected_slug");
 
-            document.querySelector("#tambah-keranjang").addEventListener("click", function() {
-                let quantity = document.getElementById("quantity").value;
-                jumlahPesan.value = quantity;
-            })
+    document.querySelectorAll(".ukuran-item").forEach(function(item) {
+        item.addEventListener("click", function() {
+            let slug = item.getAttribute("data-slug");
+            selectedSlug.value = slug;
         });
-    </script>
+    });
+
+    document.querySelector("#tambah-keranjang").addEventListener("click", function() {
+        let quantity = document.getElementById("quantity").value;
+        jumlahPesan.value = quantity;
+    })
+});
+</script>
 
 @endsection
